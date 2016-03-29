4b8dac18cf79df94891ed027bf2eaab2f8f36ce1
renezooro <renezooro@gmail.com>
2016-03-29 15:39:20 +0100

    ex 1 recursion

---
+++ chap_3/recursion.js
@@ -0,0 +1,8 @@
+function isEven(num){
+    if(num == 0) {return true; }
+    else if(num == 1) {return false; }
+    else {return isEven(num - 2) }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
